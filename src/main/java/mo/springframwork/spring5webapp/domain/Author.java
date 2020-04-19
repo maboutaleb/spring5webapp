7949f72ec5be677850937fba991ac88abe806284
Mahmoud <ma.aboutaleb@gmail.com>
2020-04-19 16:18:31 -0700

    Commit order

--- src/main/java/mo/springframwork/spring5webapp/domain/Author.java
+++ src/main/java/mo/springframwork/spring5webapp/domain/Author.java
@@ -1,10 +1,15 @@
 package mo.springframwork.spring5webapp.domain;
 
+import javax.persistence.*;
 import java.util.Set;
-
+@Entity
 public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String firstname;
     private String lastname;
+    @ManyToMany(mappedBy = "authors")
     private Set<Book> book;
 
     public Author() {
@@ -14,6 +19,14 @@ public class Author {
         this.firstname = firstname;
         this.lastname = lastname;
         this.book = book;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getFirstname() {
--- src/main/java/mo/springframwork/spring5webapp/domain/Book.java
+++ src/main/java/mo/springframwork/spring5webapp/domain/Book.java
@@ -1,10 +1,16 @@
 package mo.springframwork.spring5webapp.domain;
 
+import javax.persistence.*;
 import java.util.Set;
-
+@Entity
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
     private String title;
     private String isbn;
+    @ManyToMany
+    @JoinTable(name= "author_book", joinColumns = @JoinColumn(name = "book_id"), inverseJoinColumns = @JoinColumn(name = "author_id"))
     private Set<Author> authors;
 
     public Book() {
@@ -14,6 +20,14 @@ public class Book {
         this.title = title;
         this.isbn = isbn;
         this.authors = authors;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getTitle() {
